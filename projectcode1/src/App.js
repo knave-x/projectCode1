dfabdf3d741b6787f432b1fb7152e607a16447a3
knave-x <59017636+knave-x@users.noreply.github.com>
2022-12-16 09:21:49 +0300

    add server features

--- projectcode1/src/App.js
+++ projectcode1/src/App.js
@@ -1,49 +1,77 @@
 import React from "react";
 import "./App.css";
-
+import { useEffect, useState } from "react";
 
 const CLIENT_ID = "553563f04f1015b9f426";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+  const [userData,setUserData]=useState({})
+  useEffect(() => {
+    if (token) {
+      getUserData();
+    }
+  }, [token]);
 
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const codeParam = urlParams.get("code");
-  console.log("codeParam : ", codeParam);
+  async function getUserData() {
+    await fetch("https://api.github.com/user", {
+      method: "get",
 
-
-  // async function getUserData (){
-  //   await fetch("https://api.github.com/user",{
-  //     method: "GET",
-  //     // headers: {
-  //     //   "Authorization": "Bearer"+localStorage.getItem("accessToken")
-  //     // }
-
-  //   })
-  //   .then((response)=> {
-  //     return response.json();
-
-  //   })
-  //   .then((data)=> {
-  //     console.log("data test : ",data);
-  //   })
-  // }
-
-
-
- 
-
-  const loginWithGitHub = () => {
-    window.open(
-      "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID
+      headers: {
+        Authorization: "token " + localStorage.getItem("accessToken"),
+      },
+    })
+      .then((response) => {
+        console.log("response. ", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data test : ", data);
+        // userDataInfo
+        setUserData(data)
+      });
+  }
+  async function getToken(code) {
+    const response = await fetch(
+      "http://localhost:4000/getToken?" + "code=" + code,
+      {
+        method: "GET",
+        // headers: {
+        //   "Authorization": "Bearer"+localStorage.getItem("accessToken")
+        // }
+      }
     );
-  };
+    const jsonToken = await response.json();
+    const token = jsonToken.token;
+    localStorage.setItem("accessToken", token);
+    setToken(token);
+  }
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log("codeParam : ", codeParam);
+    if (codeParam) {
+      getToken(codeParam);
+    }
+  }, []);
+
+  let url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID;
+
+  // const loginWithGitHub = () => {
+  //   window.open(
+  //     "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID
+  //   );
+  // };
 
   return (
     <div className="App">
       <div className="App-header">
-        <button onClick={loginWithGitHub}>Login with GitHub</button>
+        {/* <button onClick={loginWithGitHub}>Login with GitHub</button> */}
         {/* <a onClick='https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}' > Github Login</a> */}
+        <a href={url}>github Testt</a>
+
+        {userData && userData.avatar_url}
       </div>
     </div>
   );
---
+++ projectcode1/src/pages/succes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const succes = () => {
+  return (
+    <div>
+        <h2> Test Succes</h2>
+      
+    </div>
+  )
+}
+
+export default succes
